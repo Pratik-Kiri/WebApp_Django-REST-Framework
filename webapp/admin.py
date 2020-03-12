a99dc6a8891a067f9031c798c8b68dd1ec418918
Pratik-Kiri <pratikkiri1999@gmail.com>
2020-03-12 18:32:53 +0530

    added new modules

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from . models import Employees, Managers
+from . models import Employee, Manager, Project
 
-admin.site.register(Employees)
-admin.site.register(Managers)
+admin.site.register(Employee)
+admin.site.register(Manager)
+admin.site.register(Project)
--- webapp/models.py
+++ webapp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Employees(models.Model):
+class Employee(models.Model):
     first_name = models.CharField(max_length=10)
     last_name = models.CharField(max_length=10)
     emp_id = models.IntegerField()
@@ -8,7 +8,7 @@ class Employees(models.Model):
     def __str__(self):
     	return self.first_name
 
-class Managers(models.Model):
+class Manager(models.Model):
     first_name = models.CharField(max_length=10)
     last_name = models.CharField(max_length=10)
     city = models.CharField(max_length=50)
@@ -16,3 +16,17 @@ class Managers(models.Model):
 
     def __str__(self):
     	return self.first_name
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    start_date = models.DateField(null=True, blank=True)
+    project_manager = models.ForeignKey(Manager, on_delete=models.SET_NULL, null=True,
+                                        related_name="pro_managers",
+                                        blank=True,
+                                        verbose_name="project manager")
+    allocated_hours = models.IntegerField(null=True)
+    created_timestamp = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_timestamp = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
